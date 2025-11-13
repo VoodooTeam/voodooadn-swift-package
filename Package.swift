7c84b0d37f08fc96c71cd76361bae77b2cd347a1
Circle CI <vsauce@voodoo.io>
2025-11-13 14:30:18 +0000

    Release 3.120.8

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
 )
 
 enum Constants {
-    static var voodooADNURL: String { "https://framework.voodoo-adn.com/iOS/sdk/3.11.3/VoodooAdn.zip"}
-    static var voodooADNChecksum: String { "fc096236f898a3962013395af3fc4e72908358768694d186c18601e24526c7c8" }
+    static var voodooADNURL: String { "https://framework.voodoo-adn.com/iOS/sdk/3.120.8/VoodooAdn.zip"}
+    static var voodooADNChecksum: String { "05b546522bfc8c377b88b22c4899889930bcc4c4a658724d54324cc7603289bc" }
     static var voodooADNName: String { "VoodooAdn" }
     static var OMSDKVoodooURL: String { "https://framework.voodoo-adn.com/omsdk/ios/1.5.2/OMSDK_Voodooio.zip"}
     static var OMSDKVoodooChecksum: String { "97914ad08803ee58d564d182b515b62250e0edd6f1d8360060603040ae39ad59" }
